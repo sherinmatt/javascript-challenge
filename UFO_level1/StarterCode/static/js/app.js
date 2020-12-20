d579b496bfac6b5e6f2a07ebe58c71973efed34b
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2020-12-19 19:24:27 -0500

    updates

---
+++ UFO_level1/StarterCode/static/js/app.js
@@ -0,0 +1,27 @@
+// from data.js
+var tableData = data;
+
+// CODE below
+
+//console.log(tableData);
+
+//reference -> table body
+var tbody = d3.select("tbody");
+
+// column needed parts- datatime, city, state, country, shape, comments
+//var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form-control");
+
+
+
+
+
+
+
+
+
+
+
+
