7b268cd9f7e27fff5761f1154f77dfe338a20ce7
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2020-12-19 20:28:11 -0500

    updates

--- UFO_level1/StarterCode/static/js/app.js
+++ UFO_level1/StarterCode/static/js/app.js
@@ -4,15 +4,15 @@ var tableData = data;
 
 // CODE below
 
-
 //console.log(tableData);
 
 //reference -> table body
 var tbody = d3.select("tbody");
 // column needed parts- datatime, city, state, country, shape, comments
 //var columns = ["datetime", "city", "state", "country", "shape", "comments"]
-//var button = d3.select("#filter-btn");
-//var form = d3.select("#form-control");
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form-control");
 
 
 //table
@@ -29,6 +29,12 @@ data.forEach((UFOReport) => {
     });
 
 });
+
+// event handler button
+button.on("click", runEnter);
+//event hangler form
+form.on("click", runEnter);
+
 
 
 
